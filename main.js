0d88940ca301eac1c818b905daa23c42eeca1638
Catherine Zhong <catherinezhong@ucsb.edu>
2026-01-14 15:04:52 -0800

    added light/dark toggle

---
+++ main.js
@@ -0,0 +1,19 @@
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+if(localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark');
+}
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+
+    if(body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+        toggleButton.textContent = 'sun';
+    }
+    else {
+        localStorage.setItem('theme', 'light');
+        toggleButton.setItem('theme', 'moon');
+    }
+});
